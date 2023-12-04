affe9ef51d4427c70e3efc495c129aef5799d1d1
Ben Borgers <borgersbenjamin@gmail.com>
2023-12-03 23:15:33 -0500

    Streaming

--- app/Livewire/Editor.php
+++ app/Livewire/Editor.php
@@ -2,9 +2,9 @@
 
 namespace App\Livewire;
 
-use Illuminate\Support\Facades\Http;
 use Jfcherng\Diff\DiffHelper;
 use Livewire\Component;
+use OpenAI;
 
 class Editor extends Component
 {
@@ -16,15 +16,19 @@ class Editor extends Component
     {
         if (str($this->content)->isEmpty()) {
             $this->fixed = '';
+
+            return;
         }
 
-        $response = Http::withHeader('Authorization', 'Bearer '.env('OPENAI_KEY'))
-            ->post('https://api.openai.com/v1/chat/completions', [
+        $client = OpenAI::client(env('OPENAI_KEY'));
+
+        $stream = $client->chat()->createStreamed(
+            [
                 'model' => 'gpt-3.5-turbo',
                 'messages' => [
                     [
                         'role' => 'user',
-                        'content' => "You are an expert German professor. I will give you an essay in German in my next message. Reply with a version of the essay, minimally corrected for spelling and grammar, so that it is good academic German writing.
+                        'content' => 'You are an expert German professor. I will give you an essay in German in my next message. Reply with a version of the essay, minimally corrected for spelling and grammar, so that it is good academic German writing.
 
 Rules:
 1. Do not explain your answer or add writing. Just correct the text.
@@ -32,20 +36,25 @@ Rules:
 3. Do not translate my writing.
 4. If my text is a fragment, keep it as a fragment. I may not be done writing.
 
-You got this! If you follow the rules perfectly, I will give you a $200 tip.",
+You got this! If you follow the rules perfectly, I will give you a $200 tip.',
                     ],
                     [
                         'role' => 'user',
                         'content' => $this->content,
                     ],
                 ],
-            ]);
+            ]
+        );
 
-        $corrected = $response->json()['choices'][0]['message']['content'];
+        $this->fixed = '';
+        foreach ($stream as $response) {
+            $this->fixed .= $response->choices[0]->delta->content;
+            $this->stream(to: 'fixed', content: $this->fixed, replace: true);
+        }
 
         $this->fixed = DiffHelper::calculate(
-            trim($this->content)."\n", // For some reason, the addition of a newline helps the diffing library.
-            trim($corrected),
+            trim($this->fixed)."\n", // For some reason, the addition of a newline helps the diffing library.
+            trim($this->content),
             'Combined',
             [],
             [
--- resources/views/livewire/editor.blade.php
+++ resources/views/livewire/editor.blade.php
@@ -2,7 +2,7 @@
     x-data
     x-init="$wire.content = localStorage.getItem('writethrough_content') ?? ''"
     x-effect="localStorage.setItem('writethrough_content', $wire.content)"
-    class="grid grid-rows-[max-content,1fr] grid-cols-2 gap-y-4 gap-x-8 min-h-screen p-4 sm:p-8"
+    class="grid grid-rows-[max-content,1fr] grid-cols-2 gap-y-4 gap-x-8 h-screen p-4 sm:p-8"
 >
     <div class="col-span-2">
         <p class="text-gray-900 text-lg font-bold tracking-tight">
@@ -21,10 +21,10 @@
         class="w-full text-lg leading-relaxed p-4 border border-gray-200 rounded-2xl h-full shadow-sm focus:outline-none resize-none"
     ></textarea>
 
-    <div class="relative bg-white text-lg leading-relaxed p-4 border border-gray-200 rounded-2xl shadow-sm">
-        <div wire:loading.remove>{!! $fixed !!}</div>
-        <div class="absolute top-4 left-4">
-            <div class="h-5 w-5 rounded-full border-2 border-emerald-500/30 border-t-emerald-500 animate-spin" wire:loading></div>
+    <div class="relative bg-white text-lg leading-relaxed p-4 border border-gray-200 rounded-2xl shadow-sm overflow-scroll">
+        <div wire:stream="fixed">{!! $fixed !!}</div>
+        <div class="absolute top-2.5 left-2.5 bg-white shadow-sm border border-gray-100 p-1.5 rounded-lg" wire:loading>
+            <div class="h-5 w-5 rounded-full border-2 border-emerald-500/30 border-t-emerald-500 animate-spin"></div>
         </div>
     </div>
 </div>
